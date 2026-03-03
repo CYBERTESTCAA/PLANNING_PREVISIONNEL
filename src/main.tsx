1f8edd1bab9ed7af555a41f7efb298ef51d3ce23
Niel-Poupelin <niel.poupelin@caa-agencement.fr>
2026-03-03 10:18:22 +0100

    fix: add crypto.randomUUID polyfill for HTTP non-localhost (MSAL)

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+// Polyfill crypto.randomUUID for non-secure contexts (HTTP on LAN)
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  crypto.randomUUID = () =>
+    '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: string) =>
+      (Number(c) ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (Number(c) / 4)))).toString(16)
+    ) as `${string}-${string}-${string}-${string}-${string}`;
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
